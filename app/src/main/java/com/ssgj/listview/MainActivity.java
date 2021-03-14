ebb1310415d008faffaf8cb48a2af62fe6c0f9f1
AIXUH23 <aixuh23@gmail.com>
2021-03-14 21:51:05 +0900

    Initial commit

---
+++ app/src/main/java/com/ssgj/listview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ssgj.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("안드로이드");
+        data.add("앱 개발");
+        data.add("자바");
+        adapter.notifyDataSetChanged();
+        
+    }
+}
